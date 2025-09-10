6c01ea945a183429189b2c5b999e0ff1a2f3ef39
Ahmad Anshori <anz507@gmail.com>
2025-09-10 18:28:43 +0700

    Stylize student form

--- resources/js/pages/Students/Index.vue
+++ resources/js/pages/Students/Index.vue
@@ -39,39 +39,43 @@ const destroy = function(student) {
 
 <template>
     <div class="p-16">
-        <h1 class="font-bold">Test Student Index</h1>
-        <div>
-            <form @submit.prevent="submit">
-                <pre>{{ form.id }}</pre>
-                <div class="flex flex-col">
-                    <label>Nama</label>
-                    <input class="border" type="text" v-model="form.nama">
-                </div>
-                <div class="flex flex-col">
-                    <label>Alamat</label>
-                    <input class="border" type="text" v-model="form.alamat">
-                </div>
-                <div class="flex flex-col">
-                    <label>Kelas ID</label>
-                    <input class="border" type="text" v-model="form.kelas_id">
-                </div>
-                <div class="flex flex-col">
-                    <label>Jenis Kelamin</label>
-                    <input class="border" type="text" v-model="form.jenis_kelamin">
-                </div>
-                <div class="flex flex-col">
-                    <label>Nama Orang Tua</label>
-                    <input class="border" type="text" v-model="form.nama_orang_tua">
-                </div>
-                <button type="submit" class="border bg-green-500 text-white px-2 py-1 rounded">Simpan</button>
-            </form>
+        <div class="p-6 rounded bg-amber-50 border border-amber-100">
+            <h1 class="font-bold">Student Form</h1>
+            <div>
+                <form @submit.prevent="submit">
+                    <div class="flex flex-col my-2">
+                        <label>Nama</label>
+                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.nama">
+                    </div>
+                    <div class="flex flex-col my-2">
+                        <label>Alamat</label>
+                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.alamat">
+                    </div>
+                    <div class="flex flex-col my-2">
+                        <label>Kelas ID</label>
+                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.kelas_id">
+                    </div>
+                    <div class="flex flex-col my-2">
+                        <label>Jenis Kelamin</label>
+                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.jenis_kelamin">
+                    </div>
+                    <div class="flex flex-col my-2">
+                        <label>Nama Orang Tua</label>
+                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.nama_orang_tua">
+                    </div>
+                    <button type="submit" class="border bg-green-500 text-white px-2 py-1 rounded">Simpan</button>
+                </form>
+            </div>
         </div>
+        <div class="text-lg font-bold mt-6">Student List</div>
         <ul>
             <template v-for="student in students">
-                <li class="text-xs my-2">
-                    {{ student.nama }} ({{ student.alamat }}): {{ student.jenis_kelamin }}
-                    <button class="border px-2 py-1 rounded bg-blue-500 text-white" @click="update(student)">Update</button>
-                    <button class="border px-2 py-1 rounded bg-red-500 text-white" @click="destroy(student)">Delete</button>
+                <li class="text-xs my-2 py-2 border-b flex justify-between items-center">
+                    <span>{{ student.nama }} ({{ student.alamat }}): {{ student.jenis_kelamin }}</span>
+                    <div>
+                        <button class="w-16 border px-2 py-1 rounded bg-blue-500 text-white" @click="update(student)">Update</button>
+                        <button class="w-16 border px-2 py-1 rounded bg-red-500 text-white" @click="destroy(student)">Delete</button>
+                    </div>
                 </li>
             </template>
         </ul>

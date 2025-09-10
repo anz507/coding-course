7340ca83e460ca414bfd786910b33c6f47bc956b
Ahmad Anshori <anz507@gmail.com>
2025-09-10 18:32:53 +0700

    Stylize student form 2

--- resources/js/pages/Students/Index.vue
+++ resources/js/pages/Students/Index.vue
@@ -38,47 +38,49 @@ const destroy = function(student) {
 </script>
 
 <template>
-    <div class="p-16">
-        <div class="p-6 rounded bg-amber-50 border border-amber-100">
-            <h1 class="font-bold">Student Form</h1>
-            <div>
-                <form @submit.prevent="submit">
-                    <div class="flex flex-col my-2">
-                        <label>Nama</label>
-                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.nama">
-                    </div>
-                    <div class="flex flex-col my-2">
-                        <label>Alamat</label>
-                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.alamat">
-                    </div>
-                    <div class="flex flex-col my-2">
-                        <label>Kelas ID</label>
-                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.kelas_id">
-                    </div>
-                    <div class="flex flex-col my-2">
-                        <label>Jenis Kelamin</label>
-                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.jenis_kelamin">
-                    </div>
-                    <div class="flex flex-col my-2">
-                        <label>Nama Orang Tua</label>
-                        <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.nama_orang_tua">
-                    </div>
-                    <button type="submit" class="border bg-green-500 text-white px-2 py-1 rounded">Simpan</button>
-                </form>
+    <AppLayout>
+        <div class="p-4">
+            <div class="p-6 rounded bg-amber-50 border border-amber-100">
+                <h1 class="font-bold">Student Form</h1>
+                <div>
+                    <form @submit.prevent="submit">
+                        <div class="flex flex-col my-2">
+                            <label>Nama</label>
+                            <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.nama">
+                        </div>
+                        <div class="flex flex-col my-2">
+                            <label>Alamat</label>
+                            <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.alamat">
+                        </div>
+                        <div class="flex flex-col my-2">
+                            <label>Kelas ID</label>
+                            <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.kelas_id">
+                        </div>
+                        <div class="flex flex-col my-2">
+                            <label>Jenis Kelamin</label>
+                            <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.jenis_kelamin">
+                        </div>
+                        <div class="flex flex-col my-2">
+                            <label>Nama Orang Tua</label>
+                            <input class="border px-2 py-1 bg-white rounded-sm" type="text" v-model="form.nama_orang_tua">
+                        </div>
+                        <button type="submit" class="border bg-green-500 text-white px-2 py-1 rounded">Simpan</button>
+                    </form>
+                </div>
             </div>
+            <div class="text-lg font-bold mt-6">Student List</div>
+            <ul>
+                <template v-for="student in students">
+                    <li class="text-xs my-2 py-2 border-b flex justify-between items-center">
+                        <span>{{ student.nama }} ({{ student.alamat }}): {{ student.jenis_kelamin }}</span>
+                        <div>
+                            <button class="w-16 border px-2 py-1 rounded bg-blue-500 text-white" @click="update(student)">Update</button>
+                            <button class="w-16 border px-2 py-1 rounded bg-red-500 text-white" @click="destroy(student)">Delete</button>
+                        </div>
+                    </li>
+                </template>
+            </ul>
         </div>
-        <div class="text-lg font-bold mt-6">Student List</div>
-        <ul>
-            <template v-for="student in students">
-                <li class="text-xs my-2 py-2 border-b flex justify-between items-center">
-                    <span>{{ student.nama }} ({{ student.alamat }}): {{ student.jenis_kelamin }}</span>
-                    <div>
-                        <button class="w-16 border px-2 py-1 rounded bg-blue-500 text-white" @click="update(student)">Update</button>
-                        <button class="w-16 border px-2 py-1 rounded bg-red-500 text-white" @click="destroy(student)">Delete</button>
-                    </div>
-                </li>
-            </template>
-        </ul>
-    </div>
+    </AppLayout>
 </template>
 
